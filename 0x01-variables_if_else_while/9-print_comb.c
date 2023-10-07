a51e768dc4792e076ce69e06fb797e7826dfc748
spektasam <spektasam@yahoo.com>
2023-10-06 20:52:57 -0700

    patience persistence perspiration success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: the main function that prints a message
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
