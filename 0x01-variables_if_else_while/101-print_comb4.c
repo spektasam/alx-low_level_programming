a2955cf12bd67b9485c5932b125a28dc3aada61b
spektasam <spektasam@yahoo.com>
2023-10-06 21:48:45 -0700

    success combination in business

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,13 +16,13 @@ int main(void)
 
 	for (d = '0'; d < '9'; d++)
 	{
-	
+
 	for (p = d + 1; p <= '9'; p++)
 	{
-	
+
 	for (q = p + 1; q <= '9'; q++)
 	{
-	if ((p != d) !=q)
+	if ((p != d) != q)
 	{
 	putchar(d);
 	putchar(p);
