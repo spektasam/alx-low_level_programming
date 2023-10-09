604c9e2b253be48f7c62cd96160e05e4f72d6cb9
spektasam <spektasam@yahoo.com>
2023-10-09 03:38:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program that prints a line with puts function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
