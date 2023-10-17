a0a501994269086c8ec7d80affee95e510808b63
spektasam <spektasam@yahoo.com>
2023-10-17 00:08:22 +0000

    report by its length defends against being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - checks length of string
+ *
+ * @s: string to be checked
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int string_length = 0;
+
+	while (s[string_length])
+		string_length++;
+	return (string_length);
+}
