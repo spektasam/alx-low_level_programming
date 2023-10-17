c3d71f4b70a318ace2dcee618d25e721d367574e
spektasam <spektasam@yahoo.com>
2023-10-17 00:56:32 +0000

    half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints characters of a string
+ *
+ * @str: the used string reference
+ *
+ * Return: always 0
+ */
+
+void puts2(char *str)
+{
+	int z = 0;
+
+	while (str[z] != '\0')
+	{
+		if (z % 2 == 0)
+		{
+			_putchar(str[z]);
+		}
+		z++;
+	}
+	_putchar('\n');
+}
